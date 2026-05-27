61bbc487e1485c882372c7157222b2313ff20cfd
brian.yu <yubrian29@gmail.com>
2026-05-27 12:51:28 +0800

    update

--- setup.sh
+++ setup.sh
@@ -1,25 +1,33 @@
-#!/bin/zsh
+# Mac OS
+#!/bin/bash
 set -e
 
+if type deactivate &> /dev/null; then
+  echo "🔁 Deactivating enviroment..."
+  deactivate
+fi
+
 if [ -d "bootcamp-env" ]; then
-  echo "🔁 Removing old virtual environment..."
+  echo "🔁 Removing old virtual environment (if any)..."
   rm -rf bootcamp-env
 fi
 
 echo "📦 Creating new virtual environment..."
-/opt/homebrew/bin/python3 -m venv bootcamp-env
+python3 -m venv bootcamp-env
+
+python3 -m pip install --upgrade pip
 
 echo "✅ Activating virtual environment..."
 source bootcamp-env/bin/activate
 
-echo "📦 Upgrading pip and installing requirements..."
 pip install --upgrade pip
 pip install -r requirements.txt
 
 python3 -m ipykernel install --user --name=bootcamp-env --display-name "Python (bootcamp-env)"
 
+# python --version
 pip --version
-python3 --version
+python --version
 jupyter --version
 
 echo "✅ Environment Setup complete."
